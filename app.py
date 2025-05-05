b0303370626034b882f50acf6d87dbae3acd5efe
Sayo <ws11579@gmail.com>
2025-05-05 19:51:59 +0800

    [feat] update args

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--checkpoint_path", type=str, default=None)
+parser.add_argument("--server_name", type=str, default="0.0.0.0")
 parser.add_argument("--port", type=int, default=7860)
 parser.add_argument("--device_id", type=int, default=0)
 parser.add_argument("--share", action='store_true', default=False)
 parser.add_argument("--bf16", action='store_true', default=True)
+parser.add_argument("--torch_compile", type=bool, default=True)
 
 args = parser.parse_args()
 os.environ["CUDA_VISIBLE_DEVICES"] = str(args.device_id)
@@ -25,7 +27,7 @@ def main(args):
         checkpoint_dir=args.checkpoint_path,
         dtype="bfloat16" if args.bf16 else "float32",
         persistent_storage_path=persistent_storage_path,
-        torch_compile=True
+        torch_compile=args.torch_compile
     )
     data_sampler = DataSampler()
 
@@ -33,7 +35,11 @@ def main(args):
         text2music_process_func=model_demo.__call__,
         sample_data_func=data_sampler.sample,
     )
-    demo.launch()
+    demo.launch(
+        server_name=args.server_name,
+        server_port=args.port,
+        share=args.share
+    )
 
 
 if __name__ == "__main__":
